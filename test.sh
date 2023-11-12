77415385e01c74a8a34474d9844b9d4d54a6ae0c
root <root@ip-172-26-14-64.ec2.internal>
2023-11-12 02:19:21 +0000

    agent script

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+if [ -f /etc/passwdnhhhgjnfhfjj ]
+
+then
+  echo exist
+else
+echo missing
+fi 
+
+if [ -d /tmp/gildas1 ]
+ then 
+  echo exist
+else
+ mkdir /tmp/gildas1
+echo new > /tmp/gildas1/file
+cat /tmp/gildas1/file
+
+
+fi
